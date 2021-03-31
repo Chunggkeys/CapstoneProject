b34c5f8db227732b69f510a609ba4ed792f94b9b
Armaan Panthaki <panthaka@mcmaster.ca>
2021-03-31 22:45:54 +0000

    Update measurementModule.py

--- measurementModule.py
+++ measurementModule.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+import spiConfig
+
 
 import spidev
 import time
@@ -38,127 +39,6 @@ spi.open(0,1)# open spi port 0, device (CS)1
 spi.max_speed_hz = (500000)
 spi.mode = (1)
 
-print ("test")
-
-count = 0
-
-def Reg_Write(address,data):    # write Fibonacci series up to 
-    for x in range(num_pins):
-        print ("Writing %s to Register %s" %( hex(data),hex(address)))
-        print(data);
-
-
-        opcode1 = (address<<2) | 0x40
-
-        GPIO.output(CS_PIN[x], False)
-        time.sleep(0.002)               # sleep for 0.1 seconds
-        GPIO.output(CS_PIN[x], True)
-        time.sleep(0.002)               # sleep for 0.1 seconds
-
-        GPIO.output(CS_PIN[x], False)
-        time.sleep(0.002)               # sleep for 0.1 seconds
-        resp = spi.xfer2([opcode1])        # transfer one byte
-        resp = spi.xfer2([data])        # transfer one byte
-        time.sleep(0.002)               # sleep for 0.1 seconds
-        GPIO.output(CS_PIN[x], True)
-    return;
-
-def Reg_read(address):    # write Fibonacci series up to
-    for x in range(num_pins):
-        print ("Reading from Register %s" %(hex(address)))
-        opcode1 = address | 0x20
-
-        GPIO.output(CS_PIN[x], False)
-        time.sleep(0.002)               # sleep for 0.1 seconds
-        GPIO.output(CS_PIN[x], True)
-        time.sleep(0.002)               # sleep for 0.1 seconds
-
-        GPIO.output(CS_PIN[x], False)
-        time.sleep(0.002)               # sleep for 0.1 seconds
-        resp = spi.xfer2([opcode1])        # transfer one byte
-        resp = spi.xfer2([0xff])        # transfer one byt
-        time.sleep(0.002)               # sleep for 0.1 seconds
-        GPIO.output(CS_PIN[x], True)
-
-        #print("Data read is %s" %(hex(resp)))
-        print(resp);
-    return;
-
-def Spi_command(command):    # write Fibonacci series up to
-    for x in range(num_pins):
-        GPIO.output(CS_PIN[x], False)
-        time.sleep(0.002)               # sleep for 0.1 seconds
-        GPIO.output(CS_PIN[x], True)
-        time.sleep(0.002)               # sleep for 0.1 seconds
-
-        GPIO.output(CS_PIN[x], False)
-        time.sleep(0.002)               # sleep for 0.1 seconds
-        resp = spi.xfer2([command])        # transfer one byte
-        time.sleep(0.002)               # sleep for 0.1 seconds
-        GPIO.output(CS_PIN[x], True)
-
-    return;
-
-
-def initialisation1():  
-    print ("INITIALISATION");
-
-
-
-    Spi_command(START);
-    time.sleep(0.1);
-    Spi_command(STOP);
-    time.sleep(0.1);
-    Spi_command(SDATAC);
-    time.sleep(0.3);
-
-    Reg_Write(0x00, 0x01);
-    time.sleep(0.01); 
-    Reg_Write(0x01, 0x04);
-    time.sleep(0.01); 
-    Reg_Write(0x02, 0x10);
-    time.sleep(0.01);
-    Reg_Write(0x03, 0x00);
-    time.sleep(0.01);
-
-    Reg_read(0x00);
-    time.sleep(0.1);
-    Reg_read(0x04);
-    time.sleep(0.1);
-    Reg_read(0x08);
-    time.sleep(0.1);
-    Reg_read(0x0c);
-
-
-
-        
-    Spi_command(RDATAC);
-    time.sleep(0.3); 
-
-    Spi_command(START);
-    time.sleep(0.1);
-     
-    for x in range(num_pins):
-        GPIO.output(CS_PIN, True)
-    return;
-
-def toHex(dec):
-    x = (dec % 16)
-    digits = "0123456789ABCDEF"
-    rest = dec / 16
-    if (rest == 0):
-        return digits[x]
-    return toHex(rest) + digits[x]
-
-def initialisation2():
-
-    time.sleep(0.2)
-    print ("STARTED")
-
-    #GPIO.output(CS_PIN, False)
-    resp = spi.xfer2([0x11])        # transfer one byte
-    time.sleep(0.3)	
-    return;
 
 def Read_Data(x):
     volt = 0;
@@ -183,11 +63,14 @@ def Read_Data(x):
     #time.sleep(0.002)               # sleep for 0.1 seconds
     GPIO.output(CS_PIN[x], True)
 
-    print("0x %x %x %x\t"%(buff[0],buff[1],buff[2]))
+   # print("0x %x %x %x\t"%(buff[0],buff[1],buff[2]))
     value = buff[0]<<16 | buff[1]<<8 | buff[2]
-    print(value)
-    volt = (float(value) * .000244140625)
-    print  (x, ":", volt,"mV")
+ #   print(value)
+    if ((float(value) >= 8388607)):
+        volt = -1*((16777216-float(value)) * (3300.0)) / (8388607)
+    else :
+        volt = ((float(value) * (3300.0)) / 1 / (8388607))
+  #  print  (x, ":", volt,"mV")
 
     #print("0x %x %x %x\t"%(buff[0],buff[1],buff[2]))
     '''print int(buff[0])
@@ -213,21 +96,20 @@ def read_R(Pot):
 
     return resistance
 
-def read_T(Pot):
+def read_T():
     resistance = 0
     voltage = Read_Data(4)
 
-    resistance= (voltage * Pot)/(VIn - voltage)
+    temp_Calib = 28.82;
+    temp = voltage/0.041276 + temp_Calib 
+    #resistance= (voltage * Pot)/(VIn - voltage)
 
-    return resistance
-
-initialisation1()
-initialisation2()
-Read_Data(0)
+    return temp
 
 while True:
 
-    x = read_R([0, 0, 0, 30000]);
-    print(x)
+    x = read_R([360,0,0,0]);
+    t = read_T()
+    print(x, "mV", t, "C" )
     time.sleep(0.5)
 
