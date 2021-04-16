71dee05d7a61da20d0d4b546c6f6140698e70a9c
Lawrence Chung <chungl1@mcmaster.ca>
2021-04-16 19:20:48 -0400

    Calibration code complete, waiting for integration testing

--- calibration.py
+++ calibration.py
@@ -1,59 +1,108 @@
-from statistics import fmean, stdev
 from math import sqrt
 
-MAX_LENGTH = 55
-OBS_LENGTH = 50
-MU_START_INDEX = 49
+CALIBRATION_MAX_LENGTH = 50
 
 class Calibration:
     def __init__(self):
-        self.queueR1 = []
-        self.queueR2 = []
-        self.queueR3 = []
-        self.queueR4 = []        
 
-        self.allResistances = [
-            self.queueR1, 
-            self.queueR2, 
-            self.queueR3, 
-            self.queueR4
-        ]
+        self.allResistanceStats = {
+            "r1" : {
+                "queue" : [],
+                "mean" : 0,
+                "var" : 0,
+                "stDev" : 0,
+                "firstCalib" : 0
+            },
+            "r2" : {
+                "queue" : [],
+                "mean" : 0,
+                "var" : 0,
+                "stDev" : 0,
+                "firstCalib" : 0
+            },
+            "r3" : {
+                "queue" : [],
+                "mean" : 0,
+                "var" : 0,
+                "stDev" : 0,
+                "firstCalib" : 0
+            },
+            "r4" : {
+                "queue" : [],
+                "mean" : 0,
+                "var" : 0,
+                "stDev" : 0,
+                "firstCalib" : 0
+            }
+        }
+
+        self.allResistors = self.allResistanceStats.keys()
 
         self.numData = 0
         self.isCalibrated = False
-        self.cstd = 0
-        self.cmean = 0
-        self.cvar = 0 
     
-    # def isCalibrated(self):
-    #     for i in self.allResistances:
-    #         muArr = i[0:OBS_LENGTH]
-    #         xArr = i[MU_START_INDEX:]
-    #         t = self._tTest(xArr, muArr)
+    def getCalibrationState(self):
+        return self.isCalibrated
+        
+    def insertCalibrationData(self, data):
+       
+        self.numData += 1
 
-    #         # Sample t value is 2, will test later
-    #         if t > 2:
-    #             return True
+        # Adds newest data to calibration data buffer for each resistor
+        # Removes the oldest data once capacity of buffer has reached CALIBRATION_MAX_LENGTH
+        index = 0 
+        for r in self.allResistors:
+            self.allResistanceStats[r]["queue"].append(data[index])
 
-    def insertDataPoint(self, data):
-        if self.numData >= MAX_LENGTH:
-            self.queueR1.pop(0)
-            self.queueR2.pop(0)
-            self.queueR3.pop(0)
-            self.queueR4.pop(0)
-        else:
-            self.numData += 1
+            if self.numData > CALIBRATION_MAX_LENGTH:
+                self.allResistanceStats[r]["firstCalib"] = self.allResistanceStats[r]["queue"].pop(0)
 
-        self.queueR1.append(data[0])
-        self.queueR2.append(data[1])
-        self.queueR3.append(data[2])
-        self.queueR4.append(data[3])
+            index += 1            
 
+        # Updates t-value every time new data is added
+        self._updateT(data)
+
+    def resetCalibrationData(self):
+        index = 0 
+        for r in self.allResistors:
+            self.allResistanceStats[r]["queue"] = []
+            index += 1            
+        self.numData = 0
     
-    def _tTest(self, xArr, muArr):
-        sampleSize = len(xArr)
-        xMean = fmean(xArr)
-        muMean = fMean(muArr)
-        stdDev = stdev(xArr)
-        t = (xMean - muMean)/(stdDev / sqrt(sampleSize))
-        return t
+    def _updateT(self, data):
+        index = 0
+        for r in self.allResistors:
+            val = data[index]
+            curMean = self.allResistanceStats[r]["mean"]
+            curVar = self.allResistanceStats[r]["var"]
+            
+            prevMean = curMean
+
+            # New mean calculation with new data
+            if self.numData <= CALIBRATION_MAX_LENGTH:
+                curMean = prevMean + (val - prevMean) / self.numData
+            else:
+                oldVal = self.allResistanceStats[r]["firstCalib"] / CALIBRATION_MAX_LENGTH
+                curMean = prevMean - oldVal + val / CALIBRATION_MAX_LENGTH
+                
+
+            self.allResistanceStats[r]["mean"] = curMean
+
+            # variance and standard deviation only calculated when there are more than
+            # two data points
+            if self.numData > 2:
+                curVar = ((self.numData-2) * curVar + (val-curMean) * (val-prevMean)) / (self.numData-1)
+                self.allResistanceStats[r]["var"] = curVar
+                self.allResistanceStats[r]["stDev"] = sqrt(curVar)
+        
+            index += 1
+        
+            # Toggles isCalibrated flag once standard deviation is greater than, equal to 2
+            if self.allResistanceStats[r]["stDev"] >= 2:
+                self.isCalibrated = True; break
+
+    def getData(self):
+        return self.allResistanceStats
+    
+    # def getNumData(self):
+    #     return self.numData
