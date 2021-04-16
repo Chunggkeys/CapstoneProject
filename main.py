20dce42916d7351c0fe1460f604e09c8749f4d7a
Lawrence Chung <chungl1@mcmaster.ca>
2021-04-15 20:29:45 -0400

    Added comments to code, setup logging functionality, setup for calibration integration

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from manager import Manager
 from sysInit import *
-# from multiprocessing import Process, Queue
 
 import threading
 import time
-# import functions
+import sys
 
 from controller import Controller
 
-# import gui
-# import hardware functions
-# import db
-
+# Port used on the Rapberry Pi, where the driver is connected to
 PORT = "/dev/ttyUSB0"
+
+# Developer mode, flip this flag to false to run with real components
+# Flip to false to use placeholder classes. 
 devMode = True
 
 IDLE_STATE                  = 0
@@ -26,7 +25,6 @@ FAILED_STATE                = -1
 
 curCycle = 1
 totalCycles = 0; displacement = 0
-calibrationDisplacement = 14
 totalDisplacement = 0
 
 # measurementData = Queue()
@@ -40,8 +38,14 @@ curState = IDLE_STATE
 startPressed = False
 dbData = {}
 
+# Set standard out to print to params.log file
+# All print statements will now print to this log
+sys.stdout = open('params.log', 'w')
+
 while 1:
     while 1:
+
+        # Loop executes until user input is submitted
         inputData = guiControl.getDataBuffer()
         if inputData is not None:
             totalCycles = inputData['nCycles']
@@ -52,25 +56,28 @@ while 1:
             break
     
     # totalDisplacement = displacement + calibrationDisplacement
-    control.setParams(totalCycles, displacement, calibrationDisplacement)
+    control.setParams(totalCycles, displacement)
 
+    # Initialize SPI communication to allow temperature and resistance
+    # measurement
     hw.initialisation1()
     hw.initialisation2()
 
+    # Start controller on another thread
     t = threading.Thread(target=control.run, args=())
     t.start()
     
     while 1:
         state = control.getCurState()
         pos = control.getPos()
-        #print("State: " + str(state))
 
         if state == MOVING_DOWN_STATE or state == MOVING_UP_STATE:
+            # As actuator is moving up and down, resistance data and temperature
+            # are being read and stored into hash
             data = hw.read_R()
             temp = hw.read_T()
 
             t = time.time()
-            # guiOutput.update(t, pos, data)
 
             dbData[dbKeys.key_time] = t
             dbData[dbKeys.key_mpos] = pos
@@ -80,83 +87,32 @@ while 1:
             dbData[dbKeys.key_res3] = data[3]
             
             db.appendData(**dbData)
+
+            # Update live graph
             guiOutput.update(t, pos, data)
+
         elif state == CALIBRATING_STATE:
+            # Calibration using statistical analysis, t-test
             data = hw.read_R()
             control.setCalibrationResistances(data)
+
         elif state == FAILED_STATE:
+            # Controller errors obtained and gui displays error message
             controllerErrors = control.getErrorBuffer()
             guiOutput.addMessage("Test has failed")
             break
+
         elif state == TEST_COMPLETE_STATE:
+            # Controller completes test, gui displays message
             guiOutput.addMessage("Test Complete!")
             break
 
     hw.close()
-            
-    # At this point, gui will have user input already
-    # This loop controls motor 
-    # while curState >= HOMING_STATE and curState <= MOVING_UP_STATE:
-    #     if curState == HOMING_STATE:
-    #         motor.home()
-    #         print(curState)
-    #         curState += 1
-    #     elif curState == CALIBRATING_STATE:
-    #         # motor.home()
-    #         # Calibration to be implemented, hardcoded at 14 mm temporarily
-    #         motor.move_absolute_mm(calibrationDisplacement)
 
-    #         print(curState)
-    #         curState += 1
-    #     elif curState == MOVING_DOWN_STATE:
-    #         # Motor displaces downward
-    #         motor.move_absolute_mm(totalDisplacement)
-
-
-    #         data = hw.read_R(devMode)
-    #         temp = hw.read_T(devMode)
-
-    #         # Change to something better
-    #         t = time.time()
-    #         pos  = motor.get_position_mm()
-    #         guiOutput.update(t, pos, data)
-
-    #         dbData[dbKeys.key_time] = t
-    #         dbData[dbKeys.key_mpos] = pos
-    #         dbData[dbKeys.key_res0] = data[0]
-    #         dbData[dbKeys.key_res1] = data[1]
-    #         dbData[dbKeys.key_res2] = data[2]
-    #         dbData[dbKeys.key_res3] = data[3]
-            
-    #         db.appendData(**dbData)
-
-    #         # Need to include error handling
-    #         if pos >= totalDisplacement:
-    #             curState += 1
-
-    #     elif curState == MOVING_UP_STATE:
-    #         # Motor displaces upward
-    #         motor.move_absolute_mm(calibrationDisplacement, waitStop=False)
-    #         if pos <= calibrationDisplacement:
-    #             if curCycle == totalCycles:
-    #                 curState = HOMING_STATE
-    #             elif curCycle < totalCycles:
-    #                 curState -= 1
-
-    #         print(curState)
-    #     elif curState == FAULTED_STATE:
-    #         # Retry most recent action?
-    #         print(curState)
-    #     elif curState == TEST_COMPLETE_STATE: 
-    #         guiOutput.addMessage("Test Complete!")
-    #         motor.home()
-    #         break
-    #     elif curState == FAILED_STATE:
-    #         print(curState)    
-    #         guiOutput.setError(msg)
-    #         curState = IDLE_STATE
-
+    # Data uploaded to database here
     db.uploadToDatabase("sample,label,here")
+
+    # Flags reset for the next test
     startPressed = False; inputData = None
     curState = IDLE_STATE; curCycle = 1
 
